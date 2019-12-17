e98c34af5c6994f032a1602c9d75a7aa946c9e02
Marco <cerasta81@gmail.com>
2019-12-17 10:03:20 -0600

    Update prompt.js

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,25 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    //create a var count to reutnr positive or negative number
+    var count = 0;
+    //turn the string into an array to iterate thru
+    var splits = str.split('');
+    for (var i = 0; i < split.length; i++){
+        //if index equals opening bracets count goes up
+        if (splits[i] === "(" && "[" && "{"){
+            count++
+        }
+        //if index equals closing bracket count goes down
+        if (spilts[i] === ")" && "]" && "}"){
+            count--
+        }
+        //if count is a even number return true else false        
+        if (count === 0){
+            return true
+        }else{
+            return false
+        }
+    }
+};
