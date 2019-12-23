49b632189e55f526e630cb866d49d86eb0db57cb
Marco <cerasta81@gmail.com>
2019-12-23 15:29:45 -0600

    military time completed

--- maxConsecutiveOnes/prompt.js
+++ maxConsecutiveOnes/prompt.js
@@ -5,19 +5,22 @@
 function maxConsecutiveOnes(array) {
   //write solution code
 // create a var count to hold how many tiems 1 happens
-  let count = 0; 
-  //run a for loop thru the array
-  for (let i = 0; i < array.length; i++){
-    //if [i] and [i + 1] count++ if [i] until chain breaks
-    if (array[i] && array[i + 1] === 1){
-      count++ 
-    }
-    // //if [i] and [i + 1] count reset and counts again
-    // if (array[i] && array[i + 1]  === 1){
-    //   count 
-    //   count++
-    // }
-  }return count;
-}
+//   let maxcoutn = 0;
+//   let count = 0; 
+//   //run a for loop thru the array
+//   for (let i = 0; i < array.length; i++){
+//     //if [i] and [i + 1] count++ if [i] until chain breaks
+//     if (array[i] && array[i + 1] === 1){
+//       count++ 
+//     }
+// //     //if [i] and [i + 1] count reset and counts again
+// //     if (array[i] && array[i + 1]  === 1){
+// //       count 
+// //       count++
+// //     }
+// //   }return count;
+// // }
 
-console.log(maxConsecutiveOnes([1,1,1,2,0,3,1,1,1,1,5,6,3]))
+// maxConsecutiveOnes([1,1,1,2,0,3,1,1,1,1,5,6,3]);
+
+
--- militaryTime/prompt.js
+++ militaryTime/prompt.js
@@ -2,5 +2,16 @@
 //Ex: militaryTime('7:47pm') returns '19:47'
 
 function militaryTime(timeStr) {
-  //write code here
+  var hr = Number(timeStr.split('')[0])
+  var mins = Number(timeStr.split('')[2] + timeStr.split('')[3] )
+  
+  if (timeStr.split('')[4] === 'p'){
+    hr += 12
+  }
+  
+    
+    return hr
 }
+
+
+console.log(militaryTime('8:10pm'));
