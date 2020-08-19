af8c7e104ddc53571c0952ece18df75dd7db05f0
kharne8 <cerasta81@gmail.com>
2020-08-19 17:16:49 -0500

    rough need polish

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -8,26 +8,30 @@
 // str: "(((10 ) ()) ((?)(:)))"   |   true
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
-// str: "[{]}"                    |   false
-const isBalanced = str => {
-    //create a var count to reutnr positive or negative number
-    var count = 0;
-    //turn the string into an array to iterate thru
-    var splits = str.split('');
-    for (var i = 0; i < split.length; i++){
-        //if index equals opening bracets count goes up
-        if (splits[i] === "(" && "[" && "{"){
-            count++
-        }
-        //if index equals closing bracket count goes down
-        if (spilts[i] === ")" && "]" && "}"){
-            count--
-        }
-        //if count is a even number return true else false        
-        if (count === 0){
-            return true
-        }else{
-            return false
-        }
+
+const isBalanced = (str) => {
+  //create a var count to return positive or negative number
+  let count = 0;
+  //turn the string into an array to iterate thru
+  let splits = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    //if index equals opening bracets count goes up
+    if (splits[i] === '(' && '[' && '{') {
+      count++;
     }
+    //if index equals closing bracket count goes down
+    if (str[i] === ')' && ']' && '}') {
+      count--;
+    }
+    //if count is a even number return true else false
+    if (count === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
+
+let example = '{[()]}';
+
+console.log(isBalanced(example));
