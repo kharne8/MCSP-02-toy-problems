10c09d4538f5e14b12e2738e8fefa6ccdb4b17b8
kharne8 <cerasta81@gmail.com>
2020-08-19 19:51:15 -0500

    solved after a couple of bumps

--- binarySearchArray/prompt.js
+++ binarySearchArray/prompt.js
@@ -10,4 +10,28 @@
  * console.log(index); // null
  */
 
-var binarySearch = function(array, target) {};
+let binarySearch = function (array, target) {
+  //find the halfway point
+  let halfPoint = array.length / 2;
+  //use halfway point to split original array into 2
+  let firstHalf = array.slice(0, halfPoint);
+  let secondHalf = array.slice(halfPoint, array.length);
+
+  //set result to null to return in case number is not found
+  let result = null;
+
+  //if found on first half return index
+  if (firstHalf.includes(target)) {
+    result = firstHalf.indexOf(target);
+    //if found on second half return index plus the length of first half to get the index from original array
+  } else if (secondHalf.includes(target)) {
+    result = secondHalf.indexOf(target) + firstHalf.length;
+  }
+
+  return result;
+};
+let arr = [20, 32, 6, 54, 100, 14, 37, 74, 56, 80, 91];
+
+console.log(binarySearch(arr, 6));
+console.log(binarySearch(arr, 74));
+console.log(binarySearch(arr, 27));
