1c520e9c96f0a84303a8694223e2978f710a2b06
kharne8 <cerasta81@gmail.com>
2020-08-21 14:00:22 -0500

    Hit a trying to figure out how to swap places but finally got it

--- bubbleSort/prompt.js
+++ bubbleSort/prompt.js
@@ -9,4 +9,33 @@
 
 // NOTE: DO NOT use JavaScript’s built-in sorting function (Array.prototype.sort).
 
-function bubbleSort(array) {}
+//I A array of numbers
+//O A array of numbers in ascending order
+//C do not use .sort
+//E none
+
+let test = [4, 3, 6, 2, 7, 1, 9, 2, 5, 8];
+
+const bubbleSort = (array) => {
+  //create a holding var for swap
+  let hold = 0;
+  //loop to cap the amount of times the array get iterated
+  for (let i = array.length; i > 0; i--) {
+    //nested for loop to check for numbers greater number
+    for (let j = 0; j < array.length; j++) {
+      //if the current j is grater than the next(j+1) than swap
+      if (array[j] > array[j + 1]) {
+        //to swap hold becmes j
+        hold = array[j];
+        //them array[j] becomes array[j+1]
+        array[j] = array[j + 1];
+        //and  finally array[j + 1] becomes hold
+        array[j + 1] = hold;
+      }
+    }
+  }
+  //finally resturn the sorted array
+  return array;
+};
+
+console.log(bubbleSort(test));
