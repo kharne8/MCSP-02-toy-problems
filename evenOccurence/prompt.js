6f70d535d4e46c3d17275dd8a8a84bcadfc29dd2
kharne8 <cerasta81@gmail.com>
2020-09-01 21:12:28 -0500

    finally done after using arrays instead of object key value pair

--- evenOccurence/prompt.js
+++ evenOccurence/prompt.js
@@ -1,3 +1,32 @@
+// var evenOccurrence = function (arr) {
+// //create a dictionary object
+// let result = null;
+// let dict = {};
+// //loop through array
+// for (let i = 0; i < arr.length; i++) {
+//   //determine if that item in the array is already in the object as a key
+//   if (dict[arr[i]]) {
+//     //if yes, increment the value
+//     result = arr[i];
+//     dict[arr[i]] = 2;
+//     //if not, add it as a key to the object with a value of 1
+//   } else {
+//     dict[arr[i]] = 1;
+//   }
+// }
+// //loop through object and return the first instance of value greater than 1
+// for (let key in dict) {
+//   if (dict[key] === 2) {
+//     if (Number(key)) {
+//       return Number(key);
+//     } else {
+//       return key;
+//     }
+//   }
+// }
+// return result;
+
+// }
 /*
  * Find the first item that occurs an even number of times in an array.
  * Remember to handle multiple even-occurrence items and return the first one.
@@ -10,31 +39,30 @@
  * console.log(onlyEven); //  4
  */
 
-var evenOccurrence = function(arr) {
-  //create a dictionary object
-  let result = null;
-  let dict = {};
-  //loop through array
-  for (let i = 0; i < arr.length; i++) {
-    //determine if that item in the array is already in the object as a key
-    if (dict[arr[i]]) {
-      //if yes, increment the value
-      result = arr[i];
-      dict[arr[i]] = 2;
-      //if not, add it as a key to the object with a value of 1
+var evenOccurrence = function (arr) {
+  let storage = [];
+  let count = 1;
+
+  arr.forEach((element) => {
+    if (!storage[element]) {
+      storage.push([element, count]);
     } else {
-      dict[arr[i]] = 1;
-    }
-  }
-  //loop through object and return the first instance of value greater than 1
-  for (let key in dict) {
-    if (dict[key] === 2) {
-      if (Number(key)) {
-        return Number(key);
-      } else {
-        return key;
+      if (storage[element]) {
+        storage.forEach((sub) => {
+          if (sub.includes(element)) {
+            sub[1]++;
+          }
+        });
       }
     }
+  });
+
+  for (let n = 0; n < storage.length; n++) {
+    if (storage[n][1] % 2 === 0) {
+      return storage[n][0];
+    }
   }
-  return result;
 };
+
+let test = [1, 7, 2, 4, 5, 6, 8, 9, 6, 4];
+console.log(evenOccurrence(test));
