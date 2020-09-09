02d658fa928db8f55686e691e265256f28b7d306
kharne8 <cerasta81@gmail.com>
2020-09-08 20:10:46 -0500

    As simple as it gets

--- filterAnimals/prompt.js
+++ filterAnimals/prompt.js
@@ -1,12 +1,22 @@
 // Given an array of objects, return the names of all animals that are older than 5 using the "filter" array method
 
 const animals = [
-  { name: "Natasha", age: 10 },
-  { name: "Raina", age: 1 },
-  { name: "Rocky", age: 13 },
-  { name: "Bullwinkle", age: 13 },
-  { name: "Mya", age: 4 },
-  { name: "Celeste", age: 1 }
+  { name: 'Natasha', age: 10 },
+  { name: 'Raina', age: 1 },
+  { name: 'Rocky', age: 13 },
+  { name: 'Bullwinkle', age: 13 },
+  { name: 'Mya', age: 4 },
+  { name: 'Celeste', age: 1 },
 ];
 
-const animalsOlderThanFive = collection => {};
+const animalsOlderThanFive = (collection) => {
+  let result = [];
+  collection.filter((element) => {
+    if (element.age > 5) {
+      result.push(element.name);
+    }
+  });
+  return result;
+};
+
+console.log(animalsOlderThanFive(animals));
