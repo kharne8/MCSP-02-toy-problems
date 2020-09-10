c01e7f0d1d5f3604214fa1edbb9b9d4934421798
kharne8 <cerasta81@gmail.com>
2020-09-09 19:26:10 -0500

    looking to improve on this one

--- fizzBuzz/prompt.js
+++ fizzBuzz/prompt.js
@@ -3,3 +3,27 @@
 // For multiples of three print “Fizz” instead of the number
 // For the multiples of five print “Buzz” instead of the number
 // For numbers which are multiples of both three and five print “FizzBuzz”
+
+const fizzBuzz = () => {
+  const result = [];
+  let count = 0;
+  while (count < 100) {
+    count++;
+    if (count % 15 === 0) {
+      result.push('FizzBuzz');
+      count++;
+    }
+    if (count % 3 === 0) {
+      result.push('Fizz');
+      count++;
+    }
+    if (count % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(count);
+    }
+  }
+  return result;
+};
+
+console.log(fizzBuzz());
